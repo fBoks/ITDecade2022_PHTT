1211be369fc8a2ea55e52194c997e2f9ba28782c
Ruslan <99635646+fBoks@users.noreply.github.com>
2022-05-18 17:23:47 +0500

    Add files via upload

---
+++ Ахмадышев_ДекадаИТ/Задание01/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Задание01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"..\..\..\Данные\задача1.txt";
+
+            string[] lines = File.ReadAllLines(path1).ToArray();
+
+            int rows = lines.Length - 1;
+            int columns = 2;
+
+            Console.WriteLine("Пожалуйста, подождите. Идёт обработка...");
+
+            // запись каждой строки в массив массивов
+            string[][] res = new string[lines.Length][];
+            int n = 0;
+            foreach (var line in lines)
+            {
+                res[n] = line.Split(' ');
+                n++;
+            }
+
+            int[,] treesCords = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    treesCords[i, j] = Convert.ToInt32(res[i + 1][j]);
+                }
+            }
+
+            SortTreesArray(treesCords);
+            FindAndPrintEmptySpace(treesCords);
+        }
+
+        private static void SortTreesArray(int[,] treesCords)
+        {
+            // сортировка (по возрастанию) по номеру ряда, сохраняя с ним и место дерева
+            int t0, t1;
+            for (int i = 0; i < treesCords.GetLength(0); i++)
+            {
+                for (int j = 0; j < treesCords.GetLength(0) - 1; j++)
+                {
+                    if (treesCords[j, 0] > treesCords[j + 1, 0])
+                    {
+                        t0 = treesCords[j + 1, 0];
+                        t1 = treesCords[j + 1, 1];
+
+                        treesCords[j + 1, 0] = treesCords[j, 0];
+                        treesCords[j + 1, 1] = treesCords[j, 1];
+
+                        treesCords[j, 0] = t0;
+                        treesCords[j, 1] = t1;
+                    }
+                }
+            }
+
+            // сортировка (по возрастанию) места дерева, если номер ряда одинаковый
+            for (int i = 0; i < treesCords.GetLength(0); i++)
+            {
+                for (int j = 0; j < treesCords.GetLength(0) - 1; j++)
+                {
+                    if (treesCords[j, 0] == treesCords[j + 1, 0])
+                    {
+                        if (treesCords[j, 1] > treesCords[j + 1, 1])
+                        {
+                            t1 = treesCords[j + 1, 1];
+
+                            treesCords[j + 1, 1] = treesCords[j, 1];
+
+                            treesCords[j, 1] = t1;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private static void FindAndPrintEmptySpace(int[,] treesCords)
+        {
+            for (int i = treesCords.GetLength(0) - 1; i >= 1; i--)
+            {
+                for (int j = treesCords.GetLength(0) - 1; j >= 1; j--)
+                {
+                    if (treesCords[j, 0] == treesCords[j - 1, 0])
+                    {
+                        if (treesCords[j, 1] - treesCords[j - 1, 1] == 14)
+                        {
+                            Console.WriteLine("Найдены 13 свободных мест для посадки деревьев.");
+                            Console.WriteLine($"Ряд: {treesCords[j, 0]} Место: {treesCords[j - 1, 1] + 1}");
+
+                            return;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Ахмадышев_ДекадаИТ/Задание02/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Задание02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int rows = Convert.ToInt32(textBox2.Text);
+                int columns = Convert.ToInt32(textBox1.Text);
+
+                dataGridView1.Rows.Clear();
+                dataGridView2.Rows.Clear();
+
+                if (columns >= 3 && columns <= 10)
+                {
+                    textBox1.BackColor = SystemColors.Window;
+                    textBox2.BackColor = SystemColors.Window;
+
+                    SetRowsAndColumnsDGV(rows, columns);
+                    SetRatings(rows, columns);
+                    CalculateAverageRatings(rows, columns);
+                }
+                else
+                {
+                    textBox1.BackColor = Color.Red;
+                    MessageBox.Show(
+                        "Количество судей вне указанного диапазона",
+                        "Ошибка",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            catch
+            {
+                textBox1.BackColor = Color.Red;
+                textBox2.BackColor = Color.Red;
+
+                MessageBox.Show(
+                    "Неверный формат ввода" + Environment.NewLine + "НЕОБХОДИМО: целое число",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.BackColor = SystemColors.Window;
+
+            textBox2.Text = "";
+            textBox2.BackColor = SystemColors.Window;
+
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.BackColor = SystemColors.Window;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.BackColor = SystemColors.Window;
+        }
+
+        double[,] ratingsArray;
+        double[] averageRatingsArray;
+
+        int columnsWidth = 60;
+
+        private void SetRowsAndColumnsDGV (int rows, int columns)
+        {
+            dataGridView1.RowCount = rows;
+            dataGridView1.ColumnCount = columns;
+
+            dataGridView2.RowCount = rows;
+            dataGridView2.ColumnCount = 1;
+
+            DataGridViewColumn column = dataGridView2.Columns[0];
+            column.Width = columnsWidth;
+        }
+
+        private void SetRatings(int rows, int columns)
+        {
+            var rnd = new Random();
+
+            ratingsArray = new double[rows, columns];
+
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    ratingsArray[i, j] = Math.Round(rnd.NextDouble(0.0, 10.0), 1);
+
+                    DataGridViewColumn column = dataGridView1.Columns[j];
+                    column.Width = columnsWidth;
+
+                    dataGridView1.Rows[i].Cells[j].Value = ratingsArray[i, j].ToString();
+                }
+            }
+        }
+
+        private void CalculateAverageRatings(int rows, int columns)
+        {
+            averageRatingsArray = new double[rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                double min = ratingsArray[i, 0];
+                double max = ratingsArray[i, 0];
+
+                for (int j = 0; j < columns; j++)
+                {
+                    if (ratingsArray[i, j] < min)
+                        min = ratingsArray[i, j];
+
+                    if (ratingsArray[i, j] > max)
+                        max = ratingsArray[i, j];
+
+                    averageRatingsArray[i] += ratingsArray[i, j]; // суммирование оценок одного фигуриста
+                }
+
+                // вычисление средней оценки для одного фигуриста
+                averageRatingsArray[i] = Math.Round(
+                    (averageRatingsArray[i] - min - max)
+                    / (columns - 2), 1);
+
+
+                dataGridView2.Rows[i].Cells[0].Value = averageRatingsArray[i].ToString();
+            }
+        }
+    }
+
+    public static class RandomExtensions
+    {
+        public static double NextDouble(this Random random, double minValue, double maxValue)
+        {
+            return random.NextDouble() * (maxValue - minValue) + minValue;
+        }
+    }
+}
